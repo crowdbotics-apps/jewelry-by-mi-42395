ac0d2e419018e49585ff85f3d988d1393815dc01
Crowdbotics <team@crowdbotics.com>
2023-07-24 19:10:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jewelry-by-mi-42395",
+  url: "https://jewelry-by-mi-42395.botics.co",
+  api: "https://jewelry-by-mi-42395.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
